7b455320c4503fcfe2a7e7b42f2a7aac758fdc83
vinodnpalli07 <vinod.nvk@gmail.com>
2021-06-28 12:28:58 -0400

    Update SerenityRunnerTest.java
    Removed unused code.

--- aspireApi/src/test/java/aspireApi/SerenityRunnerTest.java
+++ aspireApi/src/test/java/aspireApi/SerenityRunnerTest.java
@@ -14,12 +14,4 @@ import io.cucumber.junit.CucumberOptions;
   
   } 
 
-	/*
-	 * import io.cucumber.junit.Cucumber; import io.cucumber.junit.CucumberOptions;
-	 * import org.junit.runner.RunWith;
-	 * 
-	 * @RunWith(Cucumber.class)
-	 * 
-	 * @CucumberOptions() public class SerenityRunnerTest { }
-	 */
  
